4dbc25fb826135d12ca4cef8268284eac450834f
Samuel Cornet <samuel@noukod.com>
2018-12-28 20:50:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    'Hello, World!'
+  end 
+  
+  get '/name' do
+    'My name is Samuel Cornet'
+  end 
+  
+  get '/hometown' do 
+    'My hometown is Carrefour, Haiti'
+  end
+  
+  get '/favorite-song' do
+    'My favorite song is All Mighty Fortress Is Our God'
+  end
 end
